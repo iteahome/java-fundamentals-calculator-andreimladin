884c59c6756a14394123ef4144cfb80e139568ff
Andrei Mladin <andrei.mladin@gmail.com>
2019-12-23 01:49:28 +0200

    Third commit

--- src/main/java/AndreiTest.java
+++ src/main/java/AndreiTest.java
@@ -3,5 +3,6 @@ public class AndreiTest {
     public static void main(String[] args) {
         System.out.println("Hello First commit");
         System.out.println("Hello Second commit");
+        System.out.println("Hello Third commit");
     }
 }
